36fe05db3aa9ed7857291d8588f302d8b8d65965
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-19 19:12:16 -0300

    ok

---
+++ leve/src/main/java/com/leve/model/entities/Localizacao.java
@@ -0,0 +1,4 @@
+package com.leve.model.entities;
+
+public class Localizacao {
+}
---
+++ leve/src/test/java/com/leve/LeveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
