6332758e0b13c7c935a11cc16d136f94ff677004
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-19 19:21:44 -0300

    ok

--- leve/src/main/java/com/leve/model/entities/Cliente.java
+++ leve/src/main/java/com/leve/model/entities/Cliente.java
@@ -1,4 +1,19 @@
 package com.leve.model.entities;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@ToString
 public class Cliente {
+    @Id
+    @Column(length = 11, unique = true)
+    private String cpf;
+    private String nome;
+    private String dataNascimento;
+    private Integer numeroPassaporte;
 }
