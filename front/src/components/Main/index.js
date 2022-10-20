c66c2af5406f88944865277bc6a465e20542cc5a
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-10-20 16:21:51 -0300

    🔄

---
+++ front/src/components/Main/index.js
@@ -0,0 +1,24 @@
+import './style.css';
+
+export default function Main() {
+    return (
+        <div className="main-app">
+            <div className='trips'>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+                <div className='trip'></div>
+
+            </div>
+        </div>
+    );
+}  
